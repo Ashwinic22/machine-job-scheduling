784a3f2c61548980e635fb596a9f7d24ba91d535
Ashwinic22 <ashwini.chavan@capgemini.com>
2022-12-08 14:28:07 +0530

    myfirstcommit

---
+++ CUT/Code/src/createList.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "../header/header.h"
+
+void createList(machine *list)
+{
+  list->head = NULL;
+  list->tail = NULL;
+}
---
+++ CUT/Code/src/displayList.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "../header/header.h"
+
+void displayList(machine *list)
+{
+  printf("\n\n~~~~~Contents of Linked list~~~~~\n");
+  job *temp = NULL;
+  
+  for(temp = list->head; temp != list->tail->next; temp = temp->next)
+  {
+    printf("%s\t%s\t%s\t%d\t%s\n\n", temp->jno, temp->jd, temp->m, temp->time, temp->client);
+  }
+}
